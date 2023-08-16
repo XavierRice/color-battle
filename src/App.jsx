eda251f45174a5f5e3958d6f1510da8b60e54af2
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-08-16 12:48:39 -0400

    Add 'count' hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import './App.css'
+import './App.css';
 
 function App() {
+	const [count, setCount] = useState(0);
 
-
-  return (
-<div>
-  <header>
-    <h1>Color Battle</h1>
-  </header>
-</div>
-  )
+	return (
+		<div>
+			<header>
+				<h1>Color Battle</h1>
+			</header>
+		</div>
+	);
 }
 
-export default App
+export default App;
